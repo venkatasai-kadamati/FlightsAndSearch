9eea7775689e188122815974a848b2761d23337e
Venkatasai Kadamati <venkatasaikadamati@gmail.com>
2023-07-28 11:23:24 +0000

    updated serverConfig 📌

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { PORT } = require("./config/serverConfig");
+
+const { PORT } = require('./config/serverConfig');
+const ApiRoutes = require('./routes/index');
+
+const db = require('./models/index');
+// const {Airplane} = require('./models/index');
 
 const setupAndStartServer = async () => {
-  // create the express object
-  const app = express();
 
-  //   common setup for many web applications that need to handle data from forms or APIs.
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+    // create the express object
+    const app = express();
 
-  app.listen(PORT, () => {
-    console.log(`Server started at ${PORT}`);
-  });
-};
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use('/api', ApiRoutes);
+
+    app.listen(PORT, async () => {
+        console.log(`Server started at ${PORT}`);
+        if(process.env.SYNC_DB) {
+            db.sequelize.sync({alter: true});
+        }
+    });
+}
 
 setupAndStartServer();
